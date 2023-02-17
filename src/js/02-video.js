fd37fad9754d0863039eb1b7620530902340f536
Yaroslav Rebrina <yaroslavrebrina@gmail.com>
2023-02-17 14:34:37 +0200

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+
+
+const iframe = document.querySelector('iframe');
+const KEY_STORAGE = "videoplayer-current-time";
+const playerWindow = new player(iframe);
+
+
+const onPlayTimeCheker = (e) => {
+        localStorage.setItem(KEY_STORAGE, JSON.stringify(e.seconds))
+}
+
+
+playerWindow.on('timeupdate', throttle(onPlayTimeCheker, 1000));
+
+const parsedTimeFromLS = JSON.parse(localStorage.getItem(KEY_STORAGE));
+    
+playerWindow.setCurrentTime(parsedTimeFromLS);
+
+try {
+    playerWindow.setCurrentTime(parsedTimeFromLS)
+} catch (error) {
+    
+};
+    
