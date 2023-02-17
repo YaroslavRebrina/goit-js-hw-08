278a4db062fe1fc89c78f07e87924733c419bc42
Yaroslav Rebrina <yaroslavrebrina@gmail.com>
2023-02-17 19:46:42 +0200

    done automatic function

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
+import throttle from "lodash.throttle";
+
+const STORAGE_KEY = "feedback-form-state";
+const form = document.querySelector(".feedback-form");
+let data = {};
+
+// зробив callback функцію яка автоматично додає нове значення, 
+//якщо з'являться додаткові інпути
+
+const updatingData = (e) => {
+    if (!Object.keys(data).includes(e.target.name)) {
+        data[e.target.name] = e.target.value
+    } else {
+        data[e.target.name] = e.target.value
+    }
+
+    console.log(data)
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data)); 
+}
+
+form.addEventListener("input", updatingData)
+
+
 
